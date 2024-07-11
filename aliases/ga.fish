d8dfe01824add4c58c9e41352038acab64968a7b
Daniil Maltsev <da-maltsev@inbox.ru>
2024-07-11 20:05:59 +0500

    upd aliases

--- aliases/ga.fish
+++ aliases/ga.fish
@@ -1,3 +1,7 @@
 function ga --wraps='git add' --description 'alias ga=git add'
-  git add $argv; 
+  if test -d .git
+     git add $argv;
+  else
+    echo "Current directory does not contain a .git folder. Cannot run git add."
+  end
 end
--- aliases/gp.fish
+++ aliases/gp.fish
@@ -1,3 +1,7 @@
-function gp --wraps='git pull' --description 'alias gp=git pull'
-  git pull $argv; 
+function gp --wraps='git pull' --description 'use git pull if current dir has .git'
+  if test -d .git
+    git pull $argv
+  else
+    echo "Current directory does not contain a .git folder. Cannot run git pull."
+  end
 end
