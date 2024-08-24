52f91c17aea3d723a10b7a4572423ef423f02601
Daniil Maltsev <da-maltsev@inbox.ru>
2024-08-24 20:48:57 +0500

    drop ubuntu extensions

--- install/desktop/gnome-extensions.sh
+++ install/desktop/gnome-extensions.sh
@@ -1,12 +1,6 @@
 yay -S python-pipx extension-manager
 pipx install gnome-extensions-cli --system-site-packages
 
-# Turn off default Ubuntu extensions
-gnome-extensions disable tiling-assistant@ubuntu.com
-gnome-extensions disable ubuntu-appindicators@ubuntu.com
-gnome-extensions disable ubuntu-dock@ubuntu.com
-gnome-extensions disable ding@rastersoft.com
-
 # Install new extensions
 gext install tactile@lundal.io
 gext install just-perfection-desktop@just-perfection
