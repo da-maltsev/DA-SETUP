1816f594022f1f6ead256feeb28ccdf29ed9d1ef
Daniil Maltsev <da-maltsev@inbox.ru>
2024-12-30 14:33:21 +0500

    ssh term fix

--- configs/fish/config.fish
+++ configs/fish/config.fish
@@ -1,4 +1,5 @@
 set SHELL /usr/bin/fish
+export TERM=xterm-256color
 if status is-interactive
     # Commands to run in interactive sessions can go here
     eval (zellij setup --generate-auto-start fish | string collect)
