a3c1595107a6c59c752fbac8b1f5e797ebc31616
Daniil Maltsev <da-maltsev@inbox.ru>
2026-05-27 16:08:01 +0500

    Add antidote + 5 zsh plugins: autosuggestions, syntax-highlighting, fzf-tab, completions, colored-man-pages

--- install/terminal.sh
+++ install/terminal.sh
@@ -2,7 +2,7 @@ set -e
 
 source ~/.local/share/da-files/install/terminal/homebrew.sh
 
-brew install fzf ripgrep bat eza btop fastfetch neovim
+brew install fzf ripgrep bat eza btop fastfetch neovim antidote
 
 source ~/.local/share/da-files/install/terminal/fonts.sh
 source ~/.local/share/da-files/install/terminal/ghostty.sh
@@ -16,4 +16,5 @@ source ~/.local/share/da-files/install/terminal/neovim.sh
 
 mkdir -p ~/.config/zsh
 cp ~/.local/share/da-files/configs/zsh/aliases.zsh ~/.config/zsh/aliases.zsh
+cp ~/.local/share/da-files/configs/zsh/.zsh_plugins.txt ~/.config/zsh/.zsh_plugins.txt
 cp ~/.local/share/da-files/configs/zsh/.zshrc ~/.zshrc
