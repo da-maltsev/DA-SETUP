1eb77caa3606c9f7d63f9fa911cfb0613fd6d9e0
Даниил Мальцев <da-maltsev@MacBook-Daniil.local>
2026-05-30 16:58:36 +0500

    Fix setup scripts: podman rosetta, starship brew, html command, antidote path, homebrew intel, fastfetch stat, fzf integration

--- install/terminal.sh
+++ install/terminal.sh
@@ -2,7 +2,7 @@ set -e
 
 source ~/.local/share/da-files/install/terminal/homebrew.sh
 
-brew install fzf ripgrep bat eza btop fastfetch neovim antidote
+brew install fzf ripgrep bat eza btop fastfetch neovim antidote starship
 
 source ~/.local/share/da-files/install/terminal/fonts.sh
 source ~/.local/share/da-files/install/terminal/ghostty.sh
@@ -10,7 +10,6 @@ source ~/.local/share/da-files/install/terminal/uv.sh
 source ~/.local/share/da-files/install/terminal/podman.sh
 source ~/.local/share/da-files/install/terminal/lazygit.sh
 source ~/.local/share/da-files/install/terminal/opencode.sh
-source ~/.local/share/da-files/install/terminal/starship.sh
 source ~/.local/share/da-files/install/terminal/fastfetch.sh
 source ~/.local/share/da-files/install/terminal/neovim.sh
 
--- install/terminal/starship.sh
+++
@@ -1,3 +0,0 @@
-curl -sS https://starship.rs/install.sh | sh -s -- -y
-
-cp ~/.local/share/da-files/configs/starship.toml ~/.config/starship.toml
